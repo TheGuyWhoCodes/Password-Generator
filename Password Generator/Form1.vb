bb9bd70c18d885d189a4c6a182815cb4078f78cf
TheGuyWhoCodes <chris@chris-a.rocks>
2014-08-07 12:27:00 -0500

    Main Code for Password Generator 1.2
    Save password is not yet implimented

---
+++ Password Generator/Form1.vb
@@ -0,0 +1,98 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        TextBox1.Text = Generatecode()
+
+    End Sub
+
+    Public Function Generatecode() As Object
+        Dim intRnd As Object
+        Dim strName As Object
+        Dim intNameLength As Object
+        Dim intLenght As Object
+        Dim strInputString As Object
+        Dim inStep As Object
+        
+
+        strInputString = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+        intLenght = Len(strInputString)
+
+        intNameLength = 8
+
+        Randomize()
+
+        strName = ""
+
+        For inStep = 1 To intNameLength
+
+            intRnd = Int((intLenght * Rnd()) + 1)
+
+            strName = strName & Mid(strInputString, intRnd, 1)
+
+        Next
+
+        Generatecode = strName
+
+    End Function
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        TextBox1.Text = Generatecode2()
+
+    End Sub
+
+    Public Function Generatecode2() As Object
+        Dim intRnd As Object
+        Dim strName As Object
+        Dim intNameLength As Object
+        Dim intLenght As Object
+        Dim strInputString As Object
+        Dim inStep As Object
+
+
+        strInputString = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$$%^&*(){}|\][;:',<.>/?=+-_"
+
+        intLenght = Len(strInputString)
+
+        intNameLength = 8
+
+        Randomize()
+
+        strName = ""
+
+        For inStep = 1 To intNameLength
+
+            intRnd = Int((intLenght * Rnd()) + 1)
+
+            strName = strName & Mid(strInputString, intRnd, 1)
+
+        Next
+
+        Generatecode2 = strName
+    End Function
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        MsgBox("Text has been copied to your clipboard")
+        Clipboard.SetDataObject(TextBox1.Text, True)
+       
+    End Sub
+
+    Private Sub Label6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label6.Click
+        Form2.Show()
+
+    End Sub
+
+    Private Sub Label7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label7.Click
+        Form3.Show()
+
+
+    End Sub
+End Class
+
+
+
+
+
+
+
+
---
+++ Password Generator/Form2.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form2
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+        Dim theWebSite As String
+        theWebSite = "http://www.Zohaibart.blogspot.com"
+        Call Shell("explorer.exe " & theWebSite, vbNormalFocus)
+    End Sub
+
+    Private Sub WebBrowser1_DocumentCompleted(ByVal sender As System.Object, ByVal e As System.Windows.Forms.WebBrowserDocumentCompletedEventArgs)
+
+    End Sub
+End Class
